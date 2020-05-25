b1c4776c58e564b408a00cad423a76dbed1446ac
Hakan Tekın <ahmethakantekin14@gmail.com>
2020-05-25 15:06:32 +0300

    Packaging bug fixed, it was dependency : UMGEditor

--- Source/CityScape/Main_Menu_Widget.h
+++ Source/CityScape/Main_Menu_Widget.h
@@ -3,7 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/UMG.h"
+#include "Runtime/UMG/Public/UMGStyle.h"
+#include "Runtime/UMG/Public/Slate/SObjectWidget.h"
+#include "Runtime/UMG/Public/IUMGModule.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
 #include "Kismet/GameplayStatics.h"
 #include "Main_Menu_Widget.generated.h"
 
